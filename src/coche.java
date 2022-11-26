234b1f5116e58d7234df5acc4d697feb5a832aed
Matias <matiastakul5@hotmail.com>
2022-11-26 00:53:22 -0300

    Project2

---
+++ src/coche.java
@@ -0,0 +1,11 @@
+public class coche {
+    int numeroP=0;
+    public void agregarP() {
+        numeroP++;
+        System.out.println("Se añadio una puerta");
+    }
+public void mostrarP() {
+    System.out.println("La cantidad de puertas es:"+numeroP);
+}
+
+}
